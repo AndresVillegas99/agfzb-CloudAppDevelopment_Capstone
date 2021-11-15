adf8940a9d135f142b816c997bb431f8a80c30bf
AndresVillegas99 <villegasa25@gmail.com>
2021-11-15 13:44:29 +0000

    Added view for dealerships

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -45,6 +45,55 @@ class CarModel(models.Model):
         return 'Name:{self.name}, dealerId:{self.dealerId},Type:{self.type}, Year:{self.year}'.format(self=self)
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
 
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+
+    def __init__(self, review, dealership, name, id, purchase, purchase_date, car_make, car_model, car_year):
+        # Dealers review
+        self.review = review
+        # Dealership
+        self.dealership = dealership
+        # Dealership Name
+        self.name = name
+        # Dealer id
+        self.id = id
+        # If it was purchased
+        self.purchase = purchase
+        # Date of purchase
+        self.purchase_date = purchase_date
+        # The car 
+        self.car_make = car_make
+        # The car model
+        self.car_model = car_model
+        # year car aws made
+        self.car_year = car_year
+        # sentiment of the review
+        self.sentiment = sentiment
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
